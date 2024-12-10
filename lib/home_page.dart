f2f5f8db63036d72a904e0f8a45041c3eb2af164
Josenilton Junior <jjpaxeco@hotmail.com>
2024-12-09 22:23:57 -0400

    Final

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,17 +11,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String respostaFinal = '';
   final dio = Dio();
+  Map<String, dynamic>? endereco;
 
-  Future<String> getHttp() async {
+  Future<Map<String, dynamic>> getHttp() async {
     final response = await dio.get('https://viacep.com.br/ws/76870740/json/');
-    return response.toString();
+    return response.data;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Página Inicial'),
+        title: Text('meu app'),
         centerTitle: true,
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
@@ -29,17 +30,18 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Text('Consumo de API DIO'),
-          ElevatedButton(
-            onPressed: () async {
-              final resultado = await getHttp();
-              setState(() {
-                respostaFinal = resultado;
-              });
-            },
-            child: Text('Consultar'),
-          ),
-          SizedBox(height: 32),
-          Text(respostaFinal),
+          ElevatedButton(onPressed: () async {
+                Map<String, dynamic>? enderecoAuxiliar = await getHttp();
+                setState(() {
+                  endereco = enderecoAuxiliar;
+                });
+              },child: Text('Consultar')),
+          SizedBox(height: 32,),
+          Text('${endereco?['logradouro'] ?? ''}'),
+          Text('${endereco?['complemento'] ?? ''}'),
+          Text('${endereco?['bairro'] ?? ''}'),
+          Text('${endereco?['localidade'] ?? ''}'),
+          Text('${endereco?['estado'] ?? ''}'),
         ],
       ),
     );
